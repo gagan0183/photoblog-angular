2cb8bcc6b16c527893368ac5264e23986ef78ce2
gagan0183 <g.deepsingh1@gmail.com>
2017-09-24 23:42:32 +0530

    entry and entry-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['app.component.css']
 })
 export class AppComponent {
-    emoji: String[];
-    constructor() {
-        this.emoji =  ['🎉', '😍', '😜', '👍'];
-    }
+    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component'; 
+import { AppComponent } from './app.component';
+import { EntryListComponent, EntryComponent } from './entries'; 
 
 @NgModule({
     imports: [BrowserModule],
-    bootstrap: [AppComponent],
-    declarations: [AppComponent]
+    bootstrap: [
+        AppComponent
+    ],
+    declarations: [
+        AppComponent,         
+        EntryComponent,
+        EntryListComponent
+    ]
 })
 export class AppModule {
 
