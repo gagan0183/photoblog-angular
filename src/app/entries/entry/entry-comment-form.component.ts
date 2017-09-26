a3edcfc21c77697b6256bc60295b942a3300777f
gagan0183 <g.deepsingh1@gmail.com>
2017-09-26 23:28:42 +0530

    submitting data

--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-entry-comment-form',
     templateUrl: 'entry-comment-form.component.html'
 })
 export class EntryComponentFormComponent {
-    name: string;
-    comment: string;
+    name: string = "";
+    comment: string = "";
+    @Output() onCommentAdded = new EventEmitter<{name: string, comment: string}>();
+
+    onSubmit() {
+        
+        let comment = { name: this.name, comment: this.comment };
+        this.onCommentAdded.emit(comment);
+    }
 }
